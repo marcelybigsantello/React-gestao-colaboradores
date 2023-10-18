994fa359eb781777f9ad1f33882ed29736b18596
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2023-10-18 01:04:28 -0300

    Criação primeiro componente Banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+
+function Banner() {
+
+    //JSX
+    return (
+        <div className="banner">
+            <img src="./imagens/banner.png"
+                alt="Imagem de banner aplicação react gestão de colaboradores">
+            </img>
+        </div>
+
+    );
+}
+
+export default Banner;
