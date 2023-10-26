17e976db083c4f734645f6c7e13fb21216f6c751
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2023-10-26 01:38:54 -0300

    Criacao componente Colaborador e estilizacao

---
+++ src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/marcelybigsantello.png' alt='Imagem Marcely Santello'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Marcely Santello</h4>
+                <h5>Analista de Sistemas Pleno</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Colaborador;
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador/Colaborador';
 import './Time.css';
 
 const Time = (props) => {
@@ -7,6 +8,8 @@ const Time = (props) => {
     return (
         <section className="time" style={ css }>
             <h3>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
         </section>
     );
 } 
