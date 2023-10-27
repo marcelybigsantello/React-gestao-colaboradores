4836b578c0694fb58aa263b98e21bafa42c895de
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2023-10-27 00:24:39 -0300

    Renderizacao condicional com operador ternario

--- src/componentes/Colaborador/Colaborador.js
+++ src/componentes/Colaborador/Colaborador.js
@@ -11,6 +11,7 @@ const Colaborador = (props) => {
             <div className='rodape'>
                 <h4>{props.nome}</h4>
                 <h5>{props.cargo}</h5>
+                <h5>{props.formacao}</h5>
             </div>
         </div>
     );
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -6,14 +6,17 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria };
 
     return (
-        (props.listaColaboradores.length > 0) && <section className="time" style={css}>
+        (props.listaColaboradores.length > 0) ? <section className="time" style={css}>
             <h3>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.listaColaboradores.map(colaborador => <Colaborador key={colaborador.nome}
                     nome={colaborador.nome}
-                    cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem} 
+                    formacao={colaborador.formacao}/>)}
             </div>
         </section>
+        : ''
     );
 }
 
